1abec34e4a4f93c871ce28596da66a9a059b446a
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-10 11:34:34 -0800

    Printf function

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /*
  * main - print the string the printf function
  * Description:  using the main function
@@ -8,6 +7,6 @@
  */
 int main(void)
 {
-  printf ("with proper grammar, but the outcome is a piece of art,\n");
-  return (0);
+  printf("with proper grammar, but the outcome is a piece of art,\n");
+  return(0);
 }
