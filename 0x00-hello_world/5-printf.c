0cbb2838047614e5bbf2f7d16036b17d47926f85
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-10 11:14:13 -0800

    Prints function

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/*
+ *This program prints 
+ * with proper grammar, but the outcome is a piece of art
+ *main prints the input in the printf function
+ *the program returns: 0
+ */
+
+int main(void)
+{
+  printf("with proper grammar, but the outcome is a piece of art\n");
+  return(0);
+}
