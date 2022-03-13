0c76207bceac7c36ed01d4906956a0b839c8d298
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 13:55:07 +0300

    Combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function prints all possible combinations
+ *
+ * Description: using the main function to print all possible combinations.
+ * the program prints all possible combinations of single-digit numbers
+ * Return: 0
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch);
+		if (ch != 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
