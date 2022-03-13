84ccc4c90a44559a9061303b53e40c8171686ec2
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 09:06:21 +0300

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -13,7 +13,11 @@ int main(void)
 {
 	char ch;
 
-	for (ch = 'a' && 'A' ; ch <= 'z' && 'Z' ; ch++)
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	for (ch = 'A' ; ch <= 'Z' ; ch++)
 	{
 		putchar(ch);
 	}
