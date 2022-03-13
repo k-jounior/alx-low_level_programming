90311f7afdbbfd9837b4fdd48ce757a097025888
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 15:26:09 +0300

    The more brains you use, the less material you need

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - printing all the posible combinations of two digits
+ *
+ * Description: using the main function to print all possible combinations
+ * the program prints 01, 02, ... 89
+ * Return: always success
+ */
+
+int main(void)
+{
+	char ch, p;
+
+	for (ch = '0'; ch <= '9'; ch++)
+	{
+		for (p = ch + 1; p <= '9'; p++)
+		{
+			if (p != ch)
+			{
+				putchar(ch);
+				putchar(p);
+
+				if (ch == '8' && p == '9')
+					continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
