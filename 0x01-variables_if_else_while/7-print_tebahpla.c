fd93ede81ac9a21204043bec3d6309c24929468c
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 10:33:00 +0300

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the main function prints the lowercase alphabet
+ *
+ * Description: using the main function to print alphabet in reverse
+ * the program prints lowercase alphabet in reverse
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'z' ; c <= 'a' ; c++)
+	{
+		putchar(c);
+	}
+	putchar('\n');
+	return (0);
+}
