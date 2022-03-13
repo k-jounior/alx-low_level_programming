7008d9a17ac584ebd832344c8998bc092e8ea545
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 13:58:05 +0300

    Combinations

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -13,10 +13,10 @@ int main(void)
 {
 	int ch;
 
-	for (ch = 0; ch <= 9; ch++)
+	for (ch = '0'; ch <= '9'; ch++)
 	{
 		putchar(ch);
-		if (ch != 9)
+		if (ch != '9')
 		{
 			putchar(',');
 			putchar(' ');
