b82690df4868a75ccc12d3b9f4507d944161503c
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 20:47:41 +0300

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,7 +13,7 @@ int main(void)
 {
 	int a, b;
 
-	for (a = 0; a <=98; a++)
+	for (a = 0; a <= 98; a++)
 	{
 		for (b = a + 1; b <= 99; b++)
 		{
@@ -23,7 +23,7 @@ int main(void)
 			putchar((b / 10) + '0');
 			putchar((b % 10) + '0');
 
-			if (a == 98 && q == 99)
+			if (a == 98 && b == 99)
 				continue;
 			
 			putchar(',');
