0ca43d4a19644c974fa70f9498349b791a7244bc
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-13 20:43:53 +0300

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time>
+
+/**
+ * main - printing all combinations of two two-digits
+ *
+ * Description: Using the main function 
+ * printing all combinations of two two-digit numbers
+ * Return: returns success
+ */
+
+int main(void)
+{
+	int a, b;
+
+	for (a = 0; a <=98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(' ');
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && q == 99)
+				continue;
+			
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
