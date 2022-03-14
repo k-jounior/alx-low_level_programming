7fdc679277086eb526a9438a5a8bc98fd98ddbd8
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-14 13:59:05 +0300

    Do what you do better... and: do more of what you do...

---
+++ 0x00-hello_world/101-print_comb4.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - printing all posible different combinations
+ *
+ * Description: using the main function
+ * this program prints all possible combinations of three digits
+ * Return: allways return success
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = '0'; a <= '9'; a++)
+	{
+		for (b = a + 1; b <= '9'; b++)
+		{
+			for (c = b + 1; c <= '9'; c++)
+			{
+				if ((b != a) != c)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(c);
+
+					if (a == '7' && b == '8')
+						continue;
+
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
