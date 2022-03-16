5751aec9cb5b44e4f3ac58c84752f9f01525c516
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-16 23:57:43 +0300

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -11,8 +11,14 @@ void times_table(void)
 {
 	int i, j;
 
-	for (i < 0; i <= 9; i++)
+	if (n > 0 && n <= 9)
 	{
-		for (j = 0; j <= 9; j++)
+		for (i = 0; i <= n; i++)
 		{
-			
+			_putchar('0');
+			for (j = 1; j <= n; j++)
+				putformat(i * j);
+			_putchar('\n');
+		}
+	}
+}
