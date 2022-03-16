1227559d4f0ad0de32af394a10bb22f23a747eea
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-16 23:16:20 +0300

    98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: integer input
+ * Description: printing all natural numbers to 98
+ * Return: success
+ */
+
+void print_to_98(int n)
+{
+	for (n < 1; n <= 98; n++)
+	{
+		return (n);
+	}
+}
