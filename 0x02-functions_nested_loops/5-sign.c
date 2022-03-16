3e3b555135d11f93f268a2c2ae48de12921fb28f
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-16 18:37:43 +0300

    Print sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - the function prints the signs
+ *
+ * Description: using the print_sign of numbers
+ * @n - is the integer that is used in our function
+ * Return: return 1, 0 or -1
+ *
+ */
+
+int print_sign(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else 
+	{
+		_putchar('1');
+		return (1);
+	}
+}
