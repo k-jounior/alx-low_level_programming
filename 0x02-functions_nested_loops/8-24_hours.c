d51ce3eb43ffbe6ad9f994873ed596177c3c725c
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-16 20:43:43 +0300

    federal agent Jack Bauer, and today is the longest day of my life

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * jack_bauer - printing every minute of the day
+ *
+ * Description: using jack_bauer function to print every minute of the day
+ * Return: success
+ */
+
+void jack_bauer(void)
+{
+	int H, M;
+
+	for (H = 0; H < 24; H++)
+	{
+		for (M = 0; M < 60; M++)
+		{
+			_putchar((H / 10) + '0');
+			_putchar((H % 10) + '0');
+			_putchar(':');
+			_putchar((M / 10) + '0');
+			_putchar((M % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
