778eb746f1ed9ae7587c6c495a6bb6ee234574e0
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-17 16:27:43 +0300

    Printing a straight line

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - printing a straigt line
+ * @n: integer input
+ * Description: printing a straight line
+ * Return: return success
+ */
+
+void print_line(int n)
+{
+	int a = 0;
+
+	if (n > 0)
+	{
+		while (a <= n)
+		{
+			_putchar('_');
+			a++;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
