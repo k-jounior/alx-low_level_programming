02e6ac6a22eacd7b6aa4be15ca34601c5c5cd3b1
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-17 13:13:24 +0300

    print more numbers

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - function to print numbers for 0 - 9
+ * Description: the function ommits 2 and 4
+ * Return: return success
+ *
+ */
+
+void print_most_numbers(void)
+{
+	char x = '0';
+
+	while (x <= '9')
+	{
+		if (x != '2' && x != '4')
+		{
+			_putchar(x);
+			x++;
+		}
+	}
+	_putchar('\n');
+}
