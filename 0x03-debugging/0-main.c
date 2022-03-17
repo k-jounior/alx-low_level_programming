f7cfef12fe2f9a15d1fb6dff18f5caf138e34ce4
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-17 09:29:29 +0300

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -11,24 +11,23 @@
  * Return: 0
  */
 
-int main(void)
+void positive_or_negative(int i)
 {
-	int n;
 
 	srand(time(0));
-	n = rand() - RAND_MAX / 2;
+	i = rand() - RAND_MAX / 2;
 
-	if (n > 0)
+	if (i > 0)
 	{
-		printf("%i is positive\n", n);
+		printf("%i is positive\n", i);
 	}
-	else if (n == 0)
+	else if (i == 0)
 	{
-		printf("%i is zero\n",  n);
+		printf("%i is zero\n",  i);
 	}
-	else if (n < 0)
+	else if (i < 0)
 	{
-		printf("%i is negative\n", n);
+		printf("%i is negative\n", i);
 	}
 	return (0);
 }
