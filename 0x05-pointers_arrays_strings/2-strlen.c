0e98a0780a12283335e612ca2e92c48071116502
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-21 23:45:34 +0300

    This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen - prints the length of a string
+ * @s: input string
+ * Return: 0
+ */
+
+int _strlen(char *s)
+{
+	int c;
+	for (c = 0; s[c] != 0; c++)
+	{
+	}
+	return (c);
+}
