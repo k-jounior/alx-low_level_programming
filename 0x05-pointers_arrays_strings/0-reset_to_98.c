12b8eefad3d9fb3306f787f88f112f392b84e2c3
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-21 21:37:38 +0300

    Pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this function updates the value of n
+ * Description: using pointers
+ * Return: 0
+ * @*n: pointer reference
+ * @n: integer value
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
