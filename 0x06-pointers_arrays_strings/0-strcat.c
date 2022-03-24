076108895c583c4b7a916c471f8383fe98f26806
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-24 18:50:48 +0300

    String copying

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenating two strings
+ * @dest: destination address
+ * @src: source address
+ * Return: Nothing
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i + j] = src[j];
+		j++;
+	}
+	i++;
+	return (dest);
+}
