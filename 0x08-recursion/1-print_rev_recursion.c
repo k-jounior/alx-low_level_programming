2dc7e71d18657e7704b08df0d9684836014587cd
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-30 17:31:57 +0300

    Print string in reverse

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -8,13 +8,8 @@
 
 void _print_rev_recursion(char *s)
 {
-	if (*s == '\0')
+	if (*s)
 	{
-		return;
-	}
-	s++;
-	_print_rev_recursion(s);
-	s--;
-	_putchar(*s);
+		_print_rev_recursion(s + 1);
 	}
 }
