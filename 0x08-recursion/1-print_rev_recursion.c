026daa9f76b0a52b7067557c4ea03c5ed4ee0d75
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-30 17:13:54 +0300

    Print string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to reverse
+ * Return: success
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		reverse(s + 1);
+	}
+}
