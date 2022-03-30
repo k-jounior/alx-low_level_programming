6ce158be0667ecf186871b6652ee1cbc5fde4eb0
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-30 22:01:10 +0300

    X raised to power of Y

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -10,15 +10,7 @@
 int _pow_recursion(int x, int y)
 {
 	if (y < 0)
-	{
 		return (-1);
-	}
-	else if (y % 2 == 0)
-	{
-		return (_pow_recursion(x, y / 2) * _pow_recursion(x, y / 2));
-	}
 	else
-	{
-		return (x *  _pow_recursion(x, y / 2));
-	}
+		return (x * _pow_recursion(x, y - 1));
 }
