6680ded1c6edbd1d21674c44691a6e694d360db8
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-03-30 23:26:53 +0300

    Prime numbers

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -8,17 +8,17 @@
 
 int is_prime_number(int n)
 {
-	int i;
+	int i = 0;
 
 	for (i = 2; i <= n / 2; i++)
 	{
-		if (n % i == 0)
+		if (n % i != 0)
 		{
-			return (0);
+			return (1);
 		}
 		else
 		{
-			return (1);
+			return (0);
 		}
 	}
 }
