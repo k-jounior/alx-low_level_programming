84523a634728b17ce2bf1bf8b5387780a9a149ef
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-04-13 17:53:01 +0300

    More on pointers

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * cap_string - capitalize words
+ * @str: string to test
+ * Return: return capitalized string
+ */
+
+char *cap_string( char *str)
+{
+	int i = 0;
+
+
---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * is_palindrome - function to check a string
+ * @s: string input to test
+ * Return: success
+ */
+
+int is_palindrome(char *s)
+{
+	int i;
