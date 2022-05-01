c3307b97d8cc72e651f34ee204a91c11ab80053a
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-05-01 10:08:48 +0300

    Binary to int

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,33 +1,36 @@
 #include "main.h"
-
 /**
- * binary_to_uint - converts binary value to unsigned int
- * @b: pointer to a string of characters
- * Return: retruns the converted number
+ * binary_to_uint - convert binary to decimal.
+ * @b: the binary number in string format
+ * Description: convert binary to decimal
+ * section header: the header of this function is header.h
+ * Return: this return the convert number.
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int value = 0;
-	unsigned int power = 0;
-	int count = 0;
+	unsigned int result, base;
+	int i;
+
+	result = 0;
+	i = 0;
+	base = 1;
 
 	if (!b)
 		return (0);
-	/* Get length of string */
-	while (b[count] != '\0')
+
+	while (*(b + i))
 	{
-		if (b[count] != '0' && b[count] != '1')
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
-		count++;
+		i++;
 	}
-	count -= 1;
-	while (count >= 0)
+	for (i--; i >= 0; i--)
 	{
-		if (b[count] == '1')
-			value += _pow_recursion(2, power);
-		power++;
-		count--;
+		if (*(b + i) == '1')
+			result = result + base;
+		base = base * 2;
 	}
-	return (value);
+
+	return (result);
 }
