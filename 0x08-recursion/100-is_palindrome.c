eec0981d4eb4adbcd9c08fbbcb7d6507e92cc6b4
Kennedy Onyango <onyangokennedy93@gmail.com>
2022-08-20 11:59:24 +0300

    Check palindrome

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -11,15 +11,16 @@ int is_palindrome(char *s)
 	int temp, rem, rev = 0;
 	temp = *s;
 
-	while(*s != 0){
+	if(*s != 0){
 		rem = *s % 10;
 		rev = rev * 10 + rem;
 		*s /= 10;
-	}
-	if(rev == temp){
-		return (0);
-	}
-	else {
+
+		if(rev == temp){
+                return (0);
+		}
+		else {
 		return (1);
+		}
 	}
 }
